9a1dd5cb30583004ad693ea73165624b8b8fd918
mark.vanderloo@gmail.com <mark.vanderloo@gmail.com@0b489465-35d9-811b-00f0-a89ca307cf5d>
2011-02-17 17:22:33 +0000

    fixed examples, found small bugs

--- examples/checkRows.R
+++ examples/checkRows.R
@@ -1,13 +1,29 @@
 # Using character vector to define contraints
-E <- editmatrix(c( "x+3*y==2*z"
-                  , "x==z"
-                  )
-                )
+E <- editmatrix(c( "x+3*y==2*z",
+                  "x==z"))
 print(E)
 
 dat <- data.frame( x = c(0,2,1)
                  , y = c(0,0,1)
                  , z = c(0,1,1)
                  )
-# valid rows?
+# valid rows? editmatrix method
 checkRows(E, dat)
+
+
+# example using the character method.
+data(women)
+e <- c("height<=71", "weight/height>=2.1")
+valid <- checkRows(e,women)
+women[valid,]
+
+# same example, using data.frame method
+ef <- data.frame(
+    name=c("rule 1", "rule 2"), 
+    edit=e, 
+    description=c("descr1","descr2"))
+checkRows(ef,women)
+
+
+
+
