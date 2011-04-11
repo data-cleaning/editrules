5161899caabcc97b615e0bba1425fe5a7178a04a
mark.vanderloo@gmail.com <mark.vanderloo@gmail.com@0b489465-35d9-811b-00f0-a89ca307cf5d>
2011-04-11 10:25:00 +0000

    some work and testing on FM algorithm

--- pkg/R/impliedEdits.R
+++ pkg/R/impliedEdits.R
@@ -14,52 +14,70 @@ implyNumericEdits <- function(E, var, assumeNormalized=TRUE){
 
     ops <- getOps(F)
     F <- F[order(getOps(F), decreasing=TRUE), ]
-    nEq <- max(which(ops== "=="))
+    nEq <- ifelse(any(ops == "=="), max(which(ops== "==")),0)
     nEdits <- length(ops)
  
     F <- cbind(as.matrix(F),getC(F))    
 
-    # substitute equalities into (in)equalities
+    # initialize derived matrix.
     A <- matrix(nrow=0, ncol=ncol(E),dimnames=list(NULL,c(vars[-jvar],"C"))) 
 
-    v <- 1:min(nEq, nEdits-1)
-    K <-  matrix(
-        apply(F[v, -jvar,drop=FALSE],2,`/`,F[v, jvar,drop=FALSE]),
-        nrow=length(v), dimnames=list(NULL, colnames(A)))
-
-    
-    op <- c()
-    for ( k in v){
-        I <- (k+1):nEdits
-        e <- F[I, jvar,drop=FALSE]
-        G <- F[I, -jvar, drop=FALSE]
-        A <- rbind(A, sapply(1:ncol(G), function(j){
-                    G[, j,drop=FALSE] - e * K[k, j] 
-                }))
-        op <- c(op, ops[I])
-    }
-    # Substitute inequalities in inequalities
-
-    v <- (1:nEdits)[-v]    
-    K <- matrix(
-            apply(F[v,-jvar,drop=FALSE], 2, '/' ,abs(F[v,jvar])),
+    # substitute equalities into (in)equalities
+    v <- nEdits+1
+    if ( nEq > 0 ){
+        v <- 1:min(nEq, nEdits-1)
+        K <-  matrix(
+            apply(F[v, -jvar,drop=FALSE],2,`/`,F[v, jvar,drop=FALSE]),
             nrow=length(v), dimnames=list(NULL, colnames(A)))
-    
-    w <- seq_along(v)
-    for ( i in w[-length(w)] ){
-        sigF <- sign(F[v[i],jvar])
-        k <- K[i,,drop=FALSE]
-        L <- lapply(w[w>i], function(j){
-                if ( sigF != sign(F[v[j], jvar]) ) k + K[j,,drop=FALSE]
-            })
-        op <- c(op, ops[v[w>i & !sapply(L, is.null)]])
-        A <- rbind(A,do.call(rbind,L))
-    }
-    #TODO split cases <, <= 
-    #TODO reduce implicit edit matrix
 
-    b <- c(A[ ,ncol(A)], getC(E1))
-    as.editmatrix(rbind(A[,-ncol(A),drop=FALSE],getMatrix(E1)), C = b, ops=op) 
+    
+        op <- c()
+        for ( k in v){
+            I <- (k+1):nEdits
+            e <- F[I, jvar,drop=FALSE]
+            G <- F[I, -jvar, drop=FALSE]
+            A <- rbind(A, sapply(1:ncol(G), function(j){
+                        G[, j,drop=FALSE] - e * K[k, j] 
+                    }))
+            op <- c(op, ops[I])
+        }
+    }
+
+    # Substitute inequalities in inequalities
+    v <- (1:nEdits)[-v]
+    if (length(v) > 0 ){    
+        K <- matrix(
+                apply(F[v,-jvar,drop=FALSE], 2, '/' ,abs(F[v,jvar])),
+                nrow=length(v), dimnames=list(NULL, colnames(A)))
+    
+        w <- seq_along(v)
+        for ( i in w[-length(w)] ){
+            sigF <- sign(F[v[i],jvar])
+            op1 <- ops[v[w[i]]]
+            k <- K[i,,drop=FALSE]
+            L <- lapply(w[w>i], function(j){
+                val <- NULL
+                if ( sigF != sign(F[v[j], jvar]) ){ 
+                    val <- k + K[j,,drop=FALSE]
+                    if ( op1 != ops[v[w[j]]] ){ 
+                        op[length(op)+1] <<- "<" 
+                    } else { 
+                        op[length(op)+1] <<- "<="
+                    }
+                }
+                return(val)
+            })
+            A <- rbind(A,do.call(rbind,L))
+        }
+    }
+    #TODO reduce implicit edit matrix
+    rownames(A) <- paste("d",1:nrow(A),sep="")
+
+    as.editmatrix(
+        rbind(A[,-ncol(A),drop=FALSE],getMatrix(E1)), 
+        C = c(A[ ,ncol(A)], getC(E1)), 
+        ops=c(op,getOps(E1))
+    ) 
 
 }
 
