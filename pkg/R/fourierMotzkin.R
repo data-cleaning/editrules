73d1aab4d4171130993b4edc428c9f03d49ff367
mark.vanderloo@gmail.com <mark.vanderloo@gmail.com@0b489465-35d9-811b-00f0-a89ca307cf5d>
2011-04-14 14:59:38 +0000

    fourier-motzkin elimination function with dynamic redundancy removal! :-D

--- pkg/R/fourierMotzkin.R
+++ pkg/R/fourierMotzkin.R
@@ -1,37 +1,59 @@
 
 
 #' @nord
-fm <- function(A,j,tol=sqrt(.Machine$double.eps)){
+fm <- function(A,j, tol=sqrt(.Machine$double.eps)){
 
     Apos <- A[A[,j] > tol, , drop=FALSE]
     Azero <- A[abs(A[,j]) < tol,,drop=FALSE]
     Aneg <- A[A[,j] < -tol, , drop=FALSE]
-    # nothing to eliminate
-    if (nrow(Apos) == 0 || nrow(Aneg) == 0 ) return(A)    
 
-    aneg <- Aneg[1,,drop=FALSE]/abs(Aneg[1,j])
-    Atop <- t(apply(Apos,1,function(a) a + a[j]*aneg ))
+    # nothing to eliminate?
+    if (nrow(Apos) == 0 || nrow(Aneg) == 0 ) 
+        return(A)    
+    
+    Atop <- A[integer(0),]
 
-    apos <- Apos[1,,drop=FALSE]/Apos[1,j]
-    Aneg <- t(apply(Aneg,1,function(a) a + abs(a[j])*apos))
-
-    return(rbind(Atop,Azero,Aneg))
-
+    Atop <- do.call(rbind, lapply(1:nrow(Aneg),function(i){   
+            aneg <- c(
+                Aneg[i,vars,drop=FALSE]/abs(Aneg[i,j]),
+                Aneg[i,-vars,drop=FALSE])
+            t(apply(Apos, 1, 
+                function(a) c(a[vars] + a[j]*aneg[vars], a[-vars] | aneg[-vars])
+            ))
+        })
+    )
+    redundant <- rowSums(Atop[,-vars,drop=FALSE]) > nEliminated + 1
+    rbind(Atop[!redundant,,drop=FALSE],Azero)
 }
 
 #' 
 #'
 #'
 #'@nord
-fourierMotzkin <- function(A){
-    I <- 1:nrow(A)
-    for ( j in 1:(min(nrow(A),ncol(A)-1)) ){
-        A[I>j,] <- fm(A[I>j,,drop=FALSE],j)
+fourierMotzkin <- function(A, J=1){
+    vars <- 1:ncol(A)
+    A <- cbind(A,diag(rep(1,nrow(A))))
+    colnames(A)[-vars] <- rownames(A)
+    nEliminated <- 0
+    for ( j in J ){
+        A <- fm(A[,,drop=FALSE],j)
+        nEliminated <- nEliminated+1
     }
     A
 }
 
 
+#E <- editmatrix(c(
+#    "4*x1 - 5*x2 - 3*x3 + z <= 0",
+#    "-x1 + x2 -x3 <= 2",
+#    "x1 + x2 + 2*x3 <= 3",
+#    "-x1 <= 0",
+#    "-x2 <= 0",
+#    "-x3 <= 0"))
+#A <- cbind(getMatrix(E), getC(E))
+
+    
+#fourierMotzkin(A,1:2)
 
 
 
