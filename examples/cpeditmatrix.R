f0b137f41eddcfc2263b12015149e28a3f79fc23
mark.vanderloo@gmail.com <mark.vanderloo@gmail.com@0b489465-35d9-811b-00f0-a89ca307cf5d>
2011-05-02 12:40:32 +0000

    more examples and testing for cp.editmatrix

--- examples/cpeditmatrix.R
+++ examples/cpeditmatrix.R
@@ -1,13 +1,58 @@
 
+# example with a single editrule
 # p = profit, c = cost, t = turnover
-# 
+E <- editmatrix(c("p + c == t"))
+cp <- cp.editmatrix(E, x=c(p=755, c=125, t=200))
+# x obviously violates E. With all weights equal, changing any variable will do.
+# first solution:
+cp$searchNext()
+# second solution:
+cp$searchNext()
+# third solution:
+cp$searchNext()
+# there are no more solution since changing more variables would increase the weight,
+# so the result of the next statement is NULL:
+cp$searchNext()
+
+# Increasing the reliability weight of turnover, yields 2 solutions:
+cp <- cp.editmatrix(E, x=c(p=755, c=125, t=200), weight=c(1,1,2))
+# first solution:
+cp$searchNext()
+# second solution:
+cp$searchNext()
+# no more solutions available:
+cp$searchNext()
+
+
+# A case with two restrictions. The second restriction demands that
+# c/t >= 0.6 (cost should be more than 60% of turnover)
 E <- editmatrix(c(
         "p + c == t",
-        "c - 0.6*t >= 0"
-    ))
+        "c - 0.6*t >= 0"))
+cp <- cp.editmatrix(E,x=c(p=755,c=125,t=200))
+# Now, there's only one solution. 
+cp$searchNext()
+# Note that the editmatrix in the solution object suggests the actual unique value.
 
-cc <- cp.editmatrix(E,x=c(p=755,c=125,t=200), weight=c(1,1,1))
-cc$searchNext()
+
+# An example with missing data.
+E <- editmatrix(c(
+    "p + c1 + c2 == t",
+    "c1 - 0.3*t >= 0",
+    "p > 0",
+    "c1 > 0",
+    "c2 > 0",
+    "t > 0"))
+cp <- cp.editmatrix(E,x=c(p=755, c1=50, c2=NA,t=200))
+# (Note that e2 is violated.)
+# There are two solutions. Both demand that c2 is adapted:
+cp$searchNext()
+cp$searchNext()
+
+
+
+
+
 
 
 
--- pkg/R/cpeditmatrix.R
+++ pkg/R/cpeditmatrix.R
@@ -1,14 +1,15 @@
 #' Localize errors in numerical data
 #'
+#'
 #' Returns a \code{\link{choicepoint}} object for error localization in numerical data.
 #' The returned choicepoint containts methods to search depth-first to the least weighted
 #' number of variables that need to be adapted so that all restrictions in E can be 
-#' satisfied.
+#' satisfied. (Generalized principle of Fellegi and Holt (1976)).
 #'
 #' The search is excecuted with a branch-and-bound algorithm, where in the left branche,
-#' a variable is assumed correct and it's value subsituted in \code{E} while in the right
+#' a variable is assumed correct and its value subsituted in \code{E}, while in the right
 #' branche a variable is assumed incorrect and eliminated from \code{E} with Fourier-Motzkin
-#' elimination.
+#' elimination. See De Waal (2003), chapter 8 for a consice description.
 #'
 #' Every call to \code{<choicepoint>$searchNext()} returns one solution \code{list}, consisting of
 #' \itemize{
@@ -16,26 +17,38 @@
 #' \item{adapt: \code{logical} indicating whether a variable should be adapted (\code{TRUE}) or not}
 #' \item{E: The \code{\link{editmatrix}} with all variables to adapt eliminated}}
 #'
-#' The choicepoint is prepared such that missing data in the input record is already
+#' Every subsequent call leads either to \code{NULL}, in which case all solutions have been found,
+#' or a new solution with a weight \code{w} not higher than the weight of the last found solution.
+#'
+#' The choicepoint is prepared such that missing data in the input record \code{x} is already
 #' set to adapt, and missing variables have been eliminated already.
 #'
+#' @title Localize errors in numerical data based on the paradigm of Fellegi and Holt.
 #'
 #' @param E an \code{\link{editmatrix}}
-#' @param x a numerical record
+#' @param x a named numerical vecor. The record for which errors will be localized.
 #' @param weight a weight vector of length x
 #'
 #' @return an object of class \code{\link{choicepoint}}. Each execution of \code{$searchNext()} yields a solution
 #'      in the form of a \code{list} (see details).
 #'
-#' @example examples/cp.editmatrix.R
+#' @example examples/cpeditmatrix.R
+#'
+#' @references 
+#' I.P. Fellegi and D. Holt (1976). A systematic approach to automatic edit and imputation. 
+#' Journal of the American Statistical Association 71, pp 17-25
+#'
+#' T. De Waal (2003) Processing of unsave and erroneous data.  PhD thesis, Erasmus Research institute 
+#' of management, Erasmus university Rotterdam. 
+#' http://www.cbs.nl/nl-NL/menu/methoden/onderzoek-methoden/onderzoeksrapporten/proefschriften/2008-proefschrift-de-waal.htm
 #' 
 #' @export
-cp.editmatrix <- function(E, x, weight){
+cp.editmatrix <- function(E, x, weight=rep(1,length(x))){
     if ( !isNormalized(E) ) E <- normalize(E)
     # missings must be adapted, others still have to be treated.
     adapt <- is.na(x)   
-    totreat <- getVars(E)[!adapt]
-    names(adapt) <- totreat
+    totreat <- names(x)[!adapt]
+    names(adapt) <- names(x)
 
     # Eliminate missing variables.
     for ( v in getVars(E)[adapt] ) E <- eliminate(E,v)
@@ -45,7 +58,6 @@ cp.editmatrix <- function(E, x, weight){
             if ( isObviouslyUnfeasable(E) || wt > wsol ) return(FALSE)
             if (length(totreat) == 0){
                 wsol <<- wt
-                rm(totreat)
                 return(TRUE)
             }
         },
