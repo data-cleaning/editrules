85cf75deb0504e7bd3f3d8bdaf1c6a7ffe5ecc57
Mark van der Loo <mark.vanderloo@gmail.com>
2011-08-31 16:52:02 +0200

    renamed now useless reduceCat.R and reduce.R

--- pkg/R/removeEmpty.R
+++ pkg/R/removeEmpty.R
@@ -4,6 +4,7 @@
 #' @keywords internal
 #' @param E an object that is coerceable to a \code{matrix}
 removeEmpty  <- function(E){
+  if (!is.editmatrix(E)) stop("Argument is not an editmatrix")
   m <- as.matrix(E)
   B <- m != 0
   vars <- (colSums(B) != 0)
