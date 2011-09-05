c598b3953a75583bd5704d339f302674650dc5d0
Edwin de Jonge <edwindjonge@gmail.com>
2011-09-05 10:05:24 +0200

    test for warning

--- pkg/inst/tests/testIsFeasible.R
+++ pkg/inst/tests/testIsFeasible.R
@@ -3,12 +3,13 @@ require(testthat)
 context("Feasibility checks")
 
 test_that("isFeasible",{
-    expect_false(isFeasible(editmatrix(c("x +y < 0","x+y>0"))))
+    expect_false(isFeasible(editmatrix(c("x +y < 0","x+y>0")), warn=FALSE))
+})
+
+test_that("isFeasible, warning",{
+    expect_warning(isFeasible(editmatrix(c("x +y < 0","x+y>0")), warn=TRUE))
 })
 
 test_that("isFeasible with 0==1",{
-    expect_false(isFeasible(editmatrix("0==1")))
+    expect_false(isFeasible(editmatrix("0==1"), warn=FALSE))
 })
-
-
-
