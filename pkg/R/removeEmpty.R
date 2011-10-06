2d73038d4e3b7d4ab15a7e30e3af819a1c4c53b5
Mark van der Loo <mark.vanderloo@gmail.com>
2011-10-06 15:34:26 +0200

    bugfix in blocks

--- pkg/R/removeEmpty.R
+++ pkg/R/removeEmpty.R
@@ -7,9 +7,8 @@ removeEmpty  <- function(E){
   if (!is.editmatrix(E)) stop("Argument is not an editmatrix")
   m <- as.matrix(E)
   B <- m != 0
-  vars <- (colSums(B) != 0)
   edits <- (rowSums(B) != 0)
-  E[edits, vars, drop=FALSE]
+  E[edits, , drop=FALSE]
 }
 
 
