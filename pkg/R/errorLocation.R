1656401a83d36f266cfd31b106565acac525c0e9
Mark van der Loo <mark.vanderloo@gmail.com>
2011-10-17 14:53:00 +0200

    localizeErrors gains arguments useBlocks and verbose

--- pkg/R/errorLocation.R
+++ pkg/R/errorLocation.R
@@ -1,8 +1,12 @@
 
-#'
-#'
-#'
-newerrorLocation <- function(adapt, status, call=sys.call(-1), user=Sys.info()["user"], timestamp=date()){
+#' Generate new errorlocation object
+#' @param adapt Logical array of same dimensions of dataset, indicating which variable have to be changed per record
+#' @param status data.frame 
+#' @call A \code{call} object
+#' @user \code{character} username
+#' @timestamp \code{character} returned by \code{date()}.
+#' @keywords internal
+newerrorlocation <- function(adapt, status, call=sys.call(-1), user=Sys.info()["user"], timestamp=date()){
     structure(
         list(
             adapt  = adapt,
@@ -11,9 +15,38 @@ newerrorLocation <- function(adapt, status, call=sys.call(-1), user=Sys.info()["
             user = user,
             timestamp = timestamp
         ),
-        class=c('errorLocation','list')
+        class=c('errorLocation')
     ) 
 }
+
+#' Print object of class errorLocation 
+#'
+#' @param x object of class errorLocation
+#' @param ... arguments to be passed to other methods
+#' @method print errorLocation
+#' @export
+print.errorLocation <- function(x,...){
+    cat("Object of class 'errorLocation' generated at",x$timestamp,'\n')
+    cat("call :", as.character(as.expression(x$call)),'\n')
+    cat("slots:",paste("$",names(x),sep=''),'\n\n')
+    
+    if ( nrow(x$adapt) <= 10 ){ 
+        cat('Values to adapt:\n')
+        print(x$adapt)
+        cat('\n','Status:\n')
+        print(x$status)
+    } else {
+        cat('Values to adapt:')
+        print(x$adapt[1:10,,drop=FALSE])
+        cat("...print truncated",'\n\n')
+        cat('\n','Status:\n')
+        print(x$status[1:10,,drop=FALSE])
+        cat("...print truncated",'\n')
+    }
+
+}
+
+
 
 
 #' Combine two objects
@@ -22,29 +55,36 @@ newerrorLocation <- function(adapt, status, call=sys.call(-1), user=Sys.info()["
 #' @param y object 2
 #' @param ... extra parameters
 #' 
-`%+` <- function(x,y,...){
-    UseMethod("`%+%`")
+`%+%` <- function(x,y,...){
+    UseMethod("%+%")
 }
 
 #' Combine two objects of class errorLocation
 #' @method `%+%` errorLocation
 #' 
-`%+%.errorLocation` <- function(x,y,copycall=TRUE,...){
-    stopifnot(is.errorLocation(y))
-    if(copycall){ 
-        call <- x$call
-    } else {
-        call <- sys.call()
-    }
+`%+%.errorLocation` <- function(x,y,...){
+   stopifnot( class(y) == 'errorLocation' )
     
     newerrorlocation(    
         adapt=x$adapt | y$adapt,
-        call = call,
-        user = x$user,
+        status=data.frame(
+            weight     = x$status$weight + y$status$weight,
+            degeneracy = x$status$degeneracy * y$status$degeneracy,
+            user       = x$status$user + y$status$user,
+            system     = x$status$system + y$status$system,
+            elapsed    = x$status$elapsed + y$status$elapsed,
+            maxDurationExceeded = x$status$maxDurationExceeded | y$status$maxDurationExceeded
+            ),
+        call = unique(c(x$call,y$call)),
+        user = unique(c(x$user,y$user)),
         timestamp=date()
     )
 
 }
 
+
+`%+%.NULL` <- function(x,y,...){
+    y
+}
 
 
--- pkg/R/localizeErrors.R
+++ pkg/R/localizeErrors.R
@@ -5,15 +5,48 @@
 #' such that all violated edits can be satisfied, without violating new ones. If there are multiple
 #' optimal (equally weighted) solutions a random solution is chosen. 
 #'
+#' For performance purposes, the edits are split in independent \code{\link{blocks}} which are processed
+#' separately. The results are summarized in the output object, causing some loss of information.
+#' For example, the number of solutions per record (degeneracy) per block is lost. To retain this 
+#' information do someting like \code{err <- list(); for ( b in blocks(E)) err <- c(err,localizeErrors(b,dat))}
+#'
+#' 
 #'
 #' @param E an object of class \code{\link{editmatrix}} or \code{\link{editarray}}
 #' @param dat a \code{data.frame} with variables in E.
+#' @param useBlocks process error localization seperatly for independent blocks in E?
+#' @param verbose print progress to screen?
 #' @param ... options to be passed to \code{\link{errorLocalizer}}
 #'
 #' @return an object of class \code{errorLocation}
 #' @example ../examples/localizeErrors.R
 #' @export
-localizeErrors <- function(E, dat, ...){
+localizeErrors <- function(E, dat, useBlocks=TRUE, verbose=FALSE, ...){
+    if ( !useBlocks ) return(localize(E,dat,...))
+
+    B  <- blocks(E)
+    n <- length(B)
+    i <- 0
+    blockCount <- NULL
+    err <- NULL
+
+    for ( b in B ){
+        if ( verbose ){
+            i <- i + 1
+            blockCount <- paste('Processing block',format(i,width=nchar(n)), 'of',n)
+        }
+        err <- err %+% localize(b, dat, verbose, pretext=blockCount, ...)
+    }
+    if (verbose) cat('\n')
+    err
+}
+
+
+
+# workhorse function for localizeErrors
+#
+#
+localize <- function(E, dat, verbose, pretext, ...){
     stopifnot(is.data.frame(dat))
     call <- sys.call()
 
@@ -37,7 +70,10 @@ localizeErrors <- function(E, dat, ...){
     degeneracy <- rep(NA,n)
     maxDurationExceeded <- logical(n)
     X <- t(dat)
+    fmt <- paste('\r%s,record %',nchar(n),'d of %d',sep="")
     for ( i in 1:n ){
+        if (verbose) cat(sprintf(fmt,pretext,i,n))  
+        # cat('\r',pretext,' record ',format(i,width=nchar(n)),' of ',n)
         r <- X[,i]
         bt <- errorLocalizer(E,r,...)
         e <- bt$searchBest()
@@ -49,21 +85,15 @@ localizeErrors <- function(E, dat, ...){
         duration[i,] <- getDuration(bt$duration)
         maxDurationExceeded <- bt$maxdurationExceeded
     }
-    structure(
-        list(
-            adapt  = err,
-            status = data.frame(
-                weight     = weight, 
-                degeneracy = degeneracy, 
-                duration,
-                maxDurationExceeded
-            ),
-            call = call,
-            user = user,
-            timestamp = date()
-        ),
-        class=c('errorLocation','list')
-    ) 
+    newerrorlocation(
+        adapt=err,
+        status = data.frame(
+            weight  = weight,
+            degeneracy=degeneracy,
+            duration,
+            maxDurationExceeded
+            )
+        )
 }
 
 # code copied from print.proc.time...
@@ -78,30 +108,4 @@ getDuration <- function(x){
     y
 }
 
-#' Print object of class errorLocation 
-#'
-#' @param x object of class errorLocation
-#' @param ... arguments to be passed to other methods
-#' @method print errorLocation
-#' @export
-print.errorLocation <- function(x,...){
-    cat("Object of class 'errorLocation' generated at",x$timestamp,'\n')
-    cat("call :", as.character(as.expression(x$call)),'\n')
-    cat("slots:",paste("$",names(x),sep=''),'\n\n')
-    
-    if ( nrow(x$adapt) <= 10 ){ 
-        cat('Values to adapt:\n')
-        print(x$adapt)
-        cat('\n','Status:\n')
-        print(x$status)
-    } else {
-        cat('Values to adapt:')
-        print(x$adapt[1:10,,drop=FALSE])
-        cat("...print truncated",'\n\n')
-        cat('\n','Status:\n')
-        print(x$status[1:10,,drop=FALSE])
-        cat("...print truncated",'\n')
-    }
-
-}
 
