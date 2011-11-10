7d5388c923053e45c7c2fe4740d7ad14ff142242
Edwin de Jonge <edwindjonge@gmail.com>
2011-11-10 14:26:45 +0100

    added test file for MIP tests"

---
+++ pkg/inst/tests/testlocalizeErrors_mip.R
@@ -0,0 +1,115 @@
+require(testthat)
+require(lpSolveAPI)
+
+context("Localize errors using MIP")
+
+test_that("localizeError_mip",{
+  Et <- editmatrix(c(
+    "p + c == t",
+    "c - 0.6*t >= 0",
+    "c>=0",
+    "p >=0"
+    )
+                   )
+  
+  x <- c(p=755,c=125,t=200)
+  
+  sol <- localize_mip_rec(Et, x)
+  expect_equal(sol$w, 1)
+  expect_equivalent(sol$adapt, c(TRUE, FALSE, FALSE))
+  expect_equivalent(sol$x_feasible, c(75, 125, 200))
+})
+
+test_that('localizeErrors works without specified weight',{
+  expect_equivalent(localizeErrors(
+    E       = editmatrix(c('x+y==z','x<1')),
+    dat     = data.frame(
+      x = c(1,1,1),
+      y = c(1,1,1),
+      z = c(1,1,1)
+      ),
+    method="mip"
+    )$adapt,
+                    matrix(c(
+                      TRUE , FALSE, FALSE,
+                      TRUE , FALSE, FALSE,
+                      TRUE , FALSE, FALSE),
+                           nrow=3,
+                           byrow=TRUE
+                           )
+                    )
+  
+  
+})
+
+
+
+test_that('localizeErrors works with single specified weight',{
+  
+  expect_equivalent(localizeErrors(
+    E       = editmatrix('x+y==z'),
+    dat     = data.frame(
+      x = c(1,1,1),
+      y = c(1,1,1),
+      z = c(1,1,1)
+      ),
+    weight  = c(1,2,2),
+    method="mip"
+    )$adapt,
+                    matrix(c(
+                      TRUE , FALSE, FALSE,
+                      TRUE , FALSE, FALSE,
+                      TRUE , FALSE, FALSE),
+                           nrow=3,
+                           byrow=TRUE
+                           )
+                    )
+  
+  
+})
+
+
+test_that('localizeErrors works with different weights per record',{
+  expect_equivalent(localizeErrors(
+    E       = editmatrix('x+y==z'),
+    dat     = data.frame(
+      x = c(1,1,1),
+      y = c(1,1,1),
+      z = c(1,1,1)
+      ),
+    weight  = matrix(c(
+      1,2,2,
+      2,1,2,
+      2,2,1),
+                     nrow=3,
+                     byrow=TRUE
+                     ),
+    method="mip"
+    )$adapt,
+                    matrix(c(
+                      TRUE , FALSE, FALSE,
+                      FALSE, TRUE , FALSE ,
+                      FALSE, FALSE, TRUE),
+                           nrow=3,
+                           byrow=TRUE
+                           )
+                    )
+  expect_that(localizeErrors(
+    E = editmatrix('x +y==z'),
+    dat     = data.frame(
+      x = c(1,1,1),
+      y = c(1,1,1),
+      z = c(1,1,1)
+      ),
+    weight  = matrix(c(
+      1,2,2,
+      2,2,1),
+                     nrow=3,
+                     byrow=TRUE
+                     ),
+    method="mip"
+    ),
+              throws_error()
+              )
+})
+
