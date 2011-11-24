b1236536f74d2d1236c999e194150653d0452a9d
Edwin de Jonge <edwindjonge@gmail.com>
2011-11-24 14:03:37 +0100

    minor changes

--- pkg/R/parseEdits.R
+++ pkg/R/parseEdits.R
@@ -145,11 +145,14 @@ hasNum <- function(e){
   }
 }
 
-# very basic test for numerical edit
+# basic test for numerical edit
 isNum <- function(e){
   if (length(e) != 3) 
     return(FALSE)  
   cmp <- deparse(e[[1]])
+  if (cmp == "==") { 
+    return(!is.character(e[[3]]) && !is.logical(e[[3]]))
+  }
   return(cmp %in% NUMCMP)
 }
 
@@ -247,11 +250,13 @@ parseMix <- function(e, editname="", numid=0){
   numid <- numid
   
   pm <- function(i){
+    # rewrite equality as inequalities
+    #e[[i]] <- rewriteEq(e[[i]])
     if (isNum(e[[i]])){
       numid <<- numid + 1
       numvar <- paste(editname, ".num.",numid,sep="")
       #replace numeric edit with generated dummy boolean edit name
-      cat[[i]] <<- parse(text=numvar)[[1]]
+      cat[[i]] <<- as.name(numvar)
       nums[numvar] <<- as.expression(e[[i]])
     } else if (!isCat(e[[i]])){
       l <- parseMix(e[[i]], numid=numid, editname=editname)
@@ -265,14 +270,57 @@ parseMix <- function(e, editname="", numid=0){
   list(cat=cat, nums=nums, numid=numid)
 }
 
-# TODO 
-# * generate the negation of a numerical edit.
-# * rewrite a '==' numerical edit as two inequalities (needed for negation)
+# e has to be an edit
+negateEdit <- function(e){
+  op <- as.character(e[[1]])
+  if (op == "!") 
+    return(e[[2]])
+  op <- switch( op
+              , "<"  = ">="
+              , "<=" = ">"
+              , ">"  = "<="
+              , ">=" = "<"
+              , "==" = "!="
+              , "!=" = "=="
+              )
+  if (is.null(op)){
+    ne <- quote(!a)
+    ne[[2]] <- e
+    e <- ne
+  } else {
+    e[[1]] <- as.symbol(op)
+  }
+  e
+}
+
+# e has to be an numerical equality!
+rewriteEq <- function(e){
+  op <- as.character(e[[1]])
+  if (!isNum(e) || op != "==") return(e)
+  eAnd <- quote(a && b)
+  eAnd[[2]] <- e
+  eAnd[[2]][[1]] <- as.symbol("<=")
+  eAnd[[3]] <- e
+  eAnd[[3]][[1]] <- as.symbol(">=")
+  eAnd
+}
 
 
 ## quick test
-# pm <- parseMix(quote(if(x>1 && x < 10 && A %in% c('a1')) y > 2), editname="e1")
+# rewriteEq(quote(x == y + 1))
+# 
+# a <- negateEdit(quote(x>2))
+# a
+# negateEdit(a)
+# 
+# a <- negateEdit(quote(A %in% "a"))
+# a
+# negateEdit(a)
+
+# pm <- parseMix(quote(if(x>1 && x < 10 && A %in% c('a1')) y == 2), editname="e1")
 # pm
 # 
-# parseCat(pm$cat, useLogical=TRUE)
+# parseCat(pm$cat)
 # lapply(pm$nums, parseNum)
+
+
