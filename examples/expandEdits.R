30260e44d19c4674918bde49e199a78edc878b36
Edwin de Jonge <edwindjonge@gmail.com>
2011-12-14 11:13:57 +0100

    expandEdits uses environment

--- examples/expandEdits.R
+++ examples/expandEdits.R
@@ -5,6 +5,8 @@ expandEdits("#var > 0", prefix="#", var = c("turnover", "profit", "employees"))
 
 expandEdits("x_i < y_j", i=1:3,j=2:3)
 
-expandEdits("sum_i(x_i) == y", i=1:3)
+#using a variable set in the parent environment
+i <- 1:5
+expandEdits("sum_i(x_i) == y")
 
 expandEdits("sum_month(x__year._month) == x__year", month=month.abb, year=2009:2011)
--- pkg/R/expandEdits.R
+++ pkg/R/expandEdits.R
@@ -4,13 +4,14 @@
 #' a utility function to define edits for multiple variables. See the examples for the syntax.
 #' @param s edit expression, can be a \code{character} or \code{expression} vector
 #' @param prefix prefix for variables to be expanded
-#' @param useSum if \code{TRUE} sum expression will be expanded
+#' @param useSum if \code{TRUE} sum expressions will be expanded
 #' @param asExpression if \code{TRUE} an \code{\link{expression}} will be returned in stead of a \code{character}
+#' @param env enviroment that will be used to find variables to be expanded
 #' @param ... variables used in the expansion
 #' @return \code{character} or \code{expression} vector with expanded expressions
 #' @export
 #' @example ../examples/expandEdits.R
-expandEdits <- function(s, prefix="_", useSum=TRUE, asExpression=is.language(s), ...){  
+expandEdits <- function(s, prefix="_", useSum=TRUE, asExpression=is.language(s), env=parent.frame(), ...){  
   #TODO replace special regex character in prefix with escaped character.
   
   force(asExpression)
@@ -26,18 +27,26 @@ expandEdits <- function(s, prefix="_", useSum=TRUE, asExpression=is.language(s),
   if (length(s) > 1){
     return(lapply(s, expandEdits, prefix=prefix, useSum=useSum, ...))
   }
-    
-  l <- list(...)
-
+  
+  l <- as.list(env)
+  vars <- list(...)
+  l[names(vars)] <- vars
+  
+  varnms <- paste(prefix,names(l), sep="")
+  used <- sapply(varnms, grepl, s)
+  varnms <- varnms[used]
+  l <- l[used]
+  
   if (useSum) {
     sumnms <- paste("\\bsum", names(l), sep=prefix)
     sumregex1 <- paste(sumnms, "\\((.+?)\\)", sep="")
     sumregex2 <- paste(sumnms, "\\((.+?)\\).+", sep="")
+    
     vars <- names(l)
     for (i in seq_along(vars)){      
       if (length(grep(sumregex2[i], s))){
         sumvars <- sub(sumregex2[i], "\\1", s)
-        sumvars <- do.call(expandEdits, append(list(s=sumvars), l[vars[i]]))
+        sumvars <- do.call(expandEdits, list(s=sumvars, env=l[vars[i]], prefix=prefix))
         sumvars <- paste(sumvars, collapse=" + ")
         s <- sub(sumregex1[i], sumvars, s)
         l[[vars[i]]] <- NULL
