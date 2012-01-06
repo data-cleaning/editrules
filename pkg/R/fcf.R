d68d4539cf4d278ab0df7cf24297e979d32d4301
Mark van der Loo <mark.vanderloo@gmail.com>
2012-01-06 08:54:48 +0100

    small fixes on fcf

--- pkg/R/fcf.R
+++ pkg/R/fcf.R
@@ -7,11 +7,12 @@
 #' @param env an environment where all editmatrices will be stored
 #'
 #' @seealso \code{\link{fcf}}
-#' keywords internal
+#' @keywords internal
 fcf.env <- function(E, vars, env){
+    if ( nrow(E) == 0 ) return(env)
     assign(paste(vars,collapse='.'),E,envir=env)
     n <- length(vars)
-    if ( n > 0 ) for ( i in 1:n ) fcf(eliminate(E,v[i]),vars[-i],env=env)
+    if ( n > 0 ) for ( i in 1:n ) fcf.env(eliminate(E,vars[i]),vars[-i],env=env)
     env
 }
 
@@ -20,11 +21,14 @@ fcf.env <- function(E, vars, env){
 #' Implements the Field Code Forest algorithm of Garfinkel et al (1986) to 
 #' derive all essentially new implicit edits from an editarray.
 #'
+#' @param E An \code{\link{editarray}}
+#'
 #' @references
 #' R.S. Garfinkel, A.S. Kunnathur and G.E. Liepins (1986). 
 #'    Optimal imputation of erroneous data: categorical data, general edits.
 #'    Operations Research 34, 744-751.
 #'
+#' @export
 fcf <- function(E){
     if ( !is.editarray(E) ) stop('Only for arguments of class editarray')
     as.list(fcf.env(E,getVars(E)))
