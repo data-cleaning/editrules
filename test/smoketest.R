7bf8607380fd1eb16ed059a7513c2884b53ba377
mark <mark.vanderloo@gmail.com>
2012-08-10 14:23:09 +0200

    added smoketest to /test

---
+++ test/smoketest.R
@@ -0,0 +1,61 @@
+
+# n : number of variables
+# m : number of blocks (< (n-3)/2)
+accountBalance <- function(n, m, all.positive=TRUE){
+   nblock <- (n-m-1) %/% m
+   varnames <- paste("x",1:n,sep="")
+   A <- matrix(0, nrow=(m+1), ncol=n)
+   for ( i in 1:m ){
+      iblock = ((i-1)*nblock+1):(i*nblock)
+      A[i,iblock] <- 1
+      A[i,(i*nblock)+1] <- -1
+   } 
+   A[m+1,seq(nblock+1,n-m,by=nblock)] <- 1
+   A[m+1,n] <- -1
+   
+   pos = character(0)
+   if ( all.positive ) pos <- paste(varnames, 0, sep=">")
+   c(
+      reduce(as.editmatrix(A)),
+      editmatrix(pos)
+   )
+}
+
+# Generate N records for variables E.
+gen_data <- function(E,N,distr=rlnorm,...){
+   vars <- getVars(E)
+   nval <- N*length(vars)
+   as.data.frame(
+      array(
+         distr(nval,...),
+         dim=c(N,length(vars)), 
+         dimnames=list(NULL, vars)
+      )
+    )
+}
+
+
+
+smoke_test <-function(N, nvar, nblocks, all.positive=TRUE, ...){
+   e <- accountBalance(nvar, nblocks,all.positive)
+   dat <- gen_data(e,N,...)
+   el1 <- localizeErrors(e,dat,verbose=TRUE)
+   el2 <- localizeErrors(e,dat,verbose=TRUE,method="mip")
+   list(el1,el2,dat,e)
+}
+
+## this shows that even a well-scaled problem may give different results between
+## B&B and MIP 
+S <- smoke_test(100,nvar=7,2,distr=rnorm)
+all(S[[1]]$status$weight == S[[2]]$status$weight)
+
+
+
+
+
+
+
+
+
+
+
