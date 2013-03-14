99eb2da7ff82f6dca4a917536fe391dcc30a42f8
Mark van der Loo <mark.vanderloo@gmail.com>
2013-03-14 15:34:10 +0100

    solved edge case in  summary.violatedEdits

--- pkg/inst/tests/testViolatedEdits.R
+++ pkg/inst/tests/testViolatedEdits.R
@@ -60,6 +60,14 @@ test_that("violation of inequalities are detected",{
     )
 })
 
+test_that("summary.violatedEdits works when no edits are violated",{
+    
+   expect_equal(summary(violatedEdits(
+        editmatrix("x+y==z"),
+        c(x=1,y=2,z=3)
+    )),NULL)
+
+})
 
 test_that("NA's in data are handled correctly by violatededits.editmatrix",{
     expect_identical(
